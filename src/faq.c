d0458e45a2b59d6cd293f14dcc54ebcf6bd052c8
sms <>
2000-05-15 19:18:10 +0000

    added remark to faq.c

--- src/faq.c
+++ src/faq.c
@@ -4,7 +4,7 @@
    <h3>FAQ</h3>
          
       Markus Mandalka has written a small FAQ concerning the use of Husky
-      Software under Linux: 
+      Software under Linux (currently only in german): 
       <a href="http://www.mandalka.de/texte/fido-linux/">http://www.mandalka.de/texte/fido-linux/</a><p> 
     <small>Last update: 15/05/2000</small>
 
