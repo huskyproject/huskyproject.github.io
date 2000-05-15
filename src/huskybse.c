ac2a129add988158d065b3652c5739f9cbee15fc
sms <>
2000-05-15 19:14:15 +0000

    added huskybse.c and faq.c

---
+++ src/huskybse.c
@@ -0,0 +1,10 @@
+#include "top.h"
+#include "navbar.h"
+
+   <h3>huskybse</h3>
+     <blockquote>Under development...
+     </blockquote>
+          
+    <small>Last update: 15/05/2000</small>
+
+#include "bottom.h"    
