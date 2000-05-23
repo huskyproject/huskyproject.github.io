866795a00b73d3a87c95a1adeb9acbe16bc87043
tobi <>
2000-05-23 14:38:53 +0000

    Added link to www.was-ist-fido.de

--- src/fido.c
+++ src/fido.c
@@ -2,9 +2,12 @@
 #include "navbar.h"
 
    <h3>What is Fido?</h3>
-     <blockquote>Under development...
+     <blockquote>
+        A nice introduction into Fidonet can be found at 
+        <A HREF="http://www.was-ist-fido.de">http://www.was-ist-fido.de</a>.
+        It is written in German, though.
      </blockquote>
           
-    <small>Last update: 17/04/2000</small>
+    <small>Last update: 23/05/2000</small>
 
 #include "bottom.h"    
