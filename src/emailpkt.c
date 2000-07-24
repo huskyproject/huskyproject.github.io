48292a1a8ce4c045b69bc935780ec93ddc77797d
german.theler <>
2000-07-24 15:53:02 +0000

    * added emailpkt project page

---
+++ src/emailpkt.c
@@ -0,0 +1,49 @@
+#include "top.h"
+#include "navbar.h"
+
+      <h3>What is it?</h3>
+      <blockquote>
+        <strong>emailpkt</strong> is a FTN mailer over e-mail. It sends bundles
+	as mime base64 attachments and reads and processes incoming mails in
+	search of packets.
+      </blockquote>
+
+      <h3>What does it need?</h3>
+      <blockquote>
+        Operating systems:
+        <ul>
+          <li>Unix (only tested in Linux)</li>
+        </ul>
+        Dependencies (with HUSKY support):
+        <ul>
+	   <li><a href="smapi.html">smapi</a>
+	   <li><a href="fidoconf.html">fidoconf</a>
+        </ul>
+        Dependencies (standalone version):
+        <ul>
+	   <li>none
+        </ul>
+	
+      </blockquote>
+
+      <h3>Downloads</h3>
+      <blockquote>
+        <table cellpadding="5" cellspacing="0" border="0">
+          <tr valign="top">
+            <td><a href="http://cvs.physcip.uni-stuttgart.de/cvs2/emailpkt-latest.tar.gz"><tt>emailpkt-latest.tar.gz</tt></a></td>
+            <td>unstable development cvs snapshot</td>
+          </tr>
+        </table>
+      </blockquote>
+
+
+      <h3>Contacting the author</h3>
+      <blockquote>
+        Maintained by German Theler. See the <a href="team.html">team page</a>
+	for details.
+      </blockquote>
+
+
+      <small>Last update: 23/07/2000</small>
+
+#include "bottom.h"
