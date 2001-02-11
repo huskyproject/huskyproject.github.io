9ae7f0c9c2340deafaa7b5a306ca0cb3349d3fa6
d.sergienko <>
2001-02-11 09:01:18 +0000

    moved bsopack to netmail utilities

--- src/navbar.h
+++ src/navbar.h
@@ -27,10 +27,11 @@
         <dd><a href="hpt.html">hpt</a>
         <dt><b>Editor</b>
         <dd><a href="msged.html">msged</a>
-        <dt><b>Netmail trackers</b>
+        <dt><b>Netmail utilities</b>
         <dd><a href="cfroute.html">cfroute</a>
         <dd><a href="lxtrack.html">lxtrack</a>
         <dd><a href="ffma.html">ffma</a>
+        <dd><a href="bsopack.html">bsopack</a>
         <dt><b>Nodelist tools</b>
         <dd><a href="promknl.html">promknl</a>
         <dd><a href="nltools.html">nltools</a>
@@ -50,7 +51,6 @@
         <dd><a href="fidostat.html">fidostat</a>
         <dd><a href="huskypnt.html">huskypnt</a>
         <dd><a href="huskmisc.html">huskmisc</a>
-        <dd><a href="bsopack.html">bsopack</a>
       </dl>
     </td>
     <td width="10" bgcolor="#CCCCCC"></td>
