64ab2daee24850fb4d53735dc55a2884f63621ba
sms <>
2001-04-05 20:44:52 +0000

    www.was-ist-fido.de now also has an english version online

--- src/fido.c
+++ src/fido.c
@@ -3,11 +3,11 @@
 
    <h3>What is Fido?</h3>
      <blockquote>
-        A nice introduction into Fidonet can be found at 
+        A nice introduction to Fidonet can be found at
         <A HREF="http://www.was-ist-fido.de">http://www.was-ist-fido.de</a>.
-        It is written in German, though.
+        It is available in english and german.
      </blockquote>
           
-    <small>Last update: 23/05/2000</small>
+    <small>Last update: 05/04/2001</small>
 
 #include "bottom.h"    
