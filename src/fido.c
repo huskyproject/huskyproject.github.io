aa6f81022981d11ab13b9e228998e3452a09f85d
sms <>
2001-04-05 20:49:46 +0000

    it is fidopoint.de which has a translation now. Sorry for the confusion. :(

--- src/fido.c
+++ src/fido.c
@@ -3,11 +3,11 @@
 
    <h3>What is Fido?</h3>
      <blockquote>
-        A nice introduction to Fidonet can be found at
+        A nice introduction into Fidonet can be found at 
         <A HREF="http://www.was-ist-fido.de">http://www.was-ist-fido.de</a>.
-        It is available in english and german.
+        It is written in German, though.
      </blockquote>
           
-    <small>Last update: 05/04/2001</small>
+    <small>Last update: 23/05/2000</small>
 
 #include "bottom.h"    
