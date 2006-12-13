4a3e07116c50f6808374bd117152e942f23001f2
Robert James Clay <jame@users.sourceforge.net>
2006-12-13 23:04:17 +0000

    Moved old news listing to the history page.

--- src/history.c
+++ src/history.c
@@ -5,6 +5,212 @@
      <blockquote>Under development...
      </blockquote>
           
-    <small>Last update: 17/04/2000</small>
+   <h3>Older news</h3>
+    <table summary="News" align="center" width="80%" border="1" bordercolor="#CCCCCC" cellpadding="5" cellspacing="1">
+
+      <tr>
+
+        <td align="center" valign="top">
+          <strong>10/11/2002</strong>
+        </td>
+        <td align="justify">
+           New release submitted: binaries for OS/2, DOS DPMI, Win32, Linux
+           (Debian and Red Hat) and sources.<br>
+           This release includes:
+           <li> smapi-2.2.1
+           <li> fidoconfig-0.14.1
+           <li> bsopack-0.2.1
+           <li> hpt-1.2.1
+           <li> hptkill-1.10.1 
+           <li> hptsqfix-1.2.1
+           <li> hpucode-1.2.1
+           <li> htick-1.0.1
+           <li> nltools-1.2.1
+           <li> sqpack-1.2.3
+           <p> See <a href=http://sourceforge.net/projects/husky>
+           http://sourceforge.net/projects/husky</a>
+        </td>
+
+      </tr><tr>
+
+        <td align="center" valign="top">
+          <strong>17/10/2002</strong>
+        </td>
+        <td align="justify">
+           "Novice developers how-to" added into
+           <a href=https://sourceforge.net/docman/index.php?group_id=1332>
+             Project: husky: Document Manager</a>
+           <p>
+           Now avaiable here:
+           <li>HPT FAQ (russian only, sorry)
+           <li>HPT with Perl support FAQ (english, russian)
+           <li>Novice developers how-to  (english, russian)
+        </td>
+
+      </tr><tr>
+
+        <td align="center" valign="top">
+          <strong>16/10/2002</strong>
+        </td>
+        <td align="justify">
+           Week prerelease (release candidate 3) submitted: see binaries
+           at <a href=http://sourceforge.net/projects/husky>
+           http://sourceforge.net/projects/husky</a>
+        </td>
+
+      </tr><tr>
+
+        <td align="center" valign="top">
+          <strong>19/09/2002</strong>
+        </td>
+        <td align="justify">
+           New stable branches:
+           <li> bsopack 0.2
+           <li> sqpack 1.2
+           <p>
+           Release candidate 2 submitted: binaries for OS/2, DOS DPMI, Win32
+           and sources:
+           <li> smapi-2.2-rc2
+           <li> fidoconf-0.14-rc2
+           <li> hpt-1.2-rc2
+           <li> htick-1.0-rc2
+           <li> bsopack-0.2-rc2
+           <li> sqpack-1.2-rc2
+        </td>
+
+      </tr><tr>
+
+        <td align="center" valign="top">
+          <strong>03/10/2002</strong>
+        </td>
+        <td align="justify">
+           Documentation suite starts:
+           <a href=https://sourceforge.net/docman/index.php?group_id=1332>
+             Project: husky: Document Manager</a>
+        </td>
+
+      </tr><tr>
+
+        <td align="center" valign="top">
+          <strong>25/06/2002</strong>
+        </td>
+        <td align="justify">
+          <ul><a href="stable.html">Start new stable branches:</a>
+            <li>smapi 2.2 (CVS tag: smapi-2_2-stable),
+            <li>fidoconfig 0.14 (fidoconfig-0_14-stable)
+            <li>hpt 1.2 (hpt-1_2-stable)
+            <li>htick 1.0 (htick-1_0-stable)
+            <li>hpucode 1.0 (hpucode-1_0-stable)
+            <li>sqpack 1.2 (sqpack-1_2-stable)
+            <li>emailpkt 0.2 (emailpkt-0_2-stable)
+          </ul>
+          <ul><a href="current.html">Current versions named now:</a>
+            <li>smapi 2.3
+            <li>fidoconfig 0.15
+            <li>hpt 1.3
+            <li>htick 1.1
+            <li>hpucode 1.1
+            <li>sqpack 1.3
+            <li>emailpkt 0.3
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>10/02/2002</strong>
+        </td>
+        <td align="justify">
+          <a href="msged.html">Msged 6.0.1</a> and
+          <a href="smapi.html">Smapi 2.0.1</a> available.
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>27/11/2001</strong>
+        </td>
+        <td align="justify">
+          <a href="msged.html">Msged 6.0.0</a> and
+          <a href="smapi.html">Smapi 2.0.0</a> available.
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>12/04/2001</strong>
+        </td>
+        <td align="justify">
+          <a href="debian.html">Debian Packages</a> available.
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>04/01/2001</strong>
+        </td>
+        <td align="justify">
+	  Maintainance release: hpt 0.9.7d, fidoconfig 0.10c
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>03/01/2001</strong>
+        </td>
+        <td align="justify">
+	  Release: <a href="fastlst.html">Fastlst</a> 2.0.3
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>31/10/2000</strong>
+        </td>
+        <td align="justify">
+           Maintainance release of hpt 0.9.7c, htick 0.9a, sqpack 1.1.0b, fidoconfig 0.10b, smapi 1.6.4b and huskybse 0.1b with lots of JAM and  other bug fixes.
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>22/10/2000</strong>
+        </td>
+        <td align="justify">
+           First stable release for EmailPkt 0.1
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>09/10/2000</strong>
+        </td>
+        <td align="justify">
+           Maintainence bugfix releases from the stable branch: hpt 0.9.7b, sqpack-1.1.0a, others.
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>18/09/2000</strong>
+        </td>
+        <td align="justify">
+           diskpoll 0.1.3 (all platforms); nltools 1.1a for DOS
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>17/09/2000</strong>
+        </td>
+        <td align="justify">
+          Maintainance release of the core tools: bug fixes for hpt and
+        nltools, JAM bug fixes, and new: support for BeOS
+        </td>
+      </tr><tr>
+        <td align="center" valign="top">
+          <strong>21/07/2000</strong>
+        </td>
+        <td align="justify">
+          Binary releases for OS/2 and Win32 of the Husky core tools hpt, fidoconf, nltools, htick and sqpack!
+        </td>
+      </tr>
+      <tr>
+        <td align="center" valign="top">
+          <strong>18/07/2000</strong>
+        </td>
+        <td align="justify">
+          New Release of the Husky core tools hpt, fidoconf, smapi, nltools, htick, sqpack and huskybse!
+        </td>
+      </tr>
+      <tr>
+        <td align="center" valign="top">
+          <strong>17/04/2000</strong>
+        </td>
+        <td align="justify">
+          New web page!
+        </td>
+      </tr>
+    </table>
+    <small>Last update: 13/12/2006</small>
 
 #include "bottom.h"    
