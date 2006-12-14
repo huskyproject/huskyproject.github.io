dbe7e460f7c56d05d0efee6ee73c47fcc9e98c84
Robert James Clay <jame@users.sourceforge.net>
2006-12-14 01:26:10 +0000

    Updated the Downloads section

--- src/huskybse.c
+++ src/huskybse.c
@@ -23,30 +23,48 @@
 
       <h3>Downloads</h3>
       <blockquote>
-        <table cellpadding="5" cellspacing="0" border="0">
+	<p>Archives of version v1.0 &amp; v1.4 available on the project&nbsp;
+	<a href="http://sourceforge.net/project/showfiles.php?group_id=1332">downloads</a>&nbsp;page.</p>
+        
+	<table cellpadding="5" cellspacing="0" border="0">
+
           <tr valign="top">
-            <td><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/huskybse-0.1b-src.tar.gz">huskybse-0.2.0-src.tar.gz</a>
-</td>
-            <td>newest release huskybse-0.2-release</td>
+            <td><b>huskybase-1.4-tar.gz</td>
+            <td>huskybase v1.4 RC4</td>
           </tr>
+
           <tr valign="top">
-            <td><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/huskybse-0.1b-src.tar.gz">huskybse-0.1b-src.tar.gz</a>
-             <br><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/huskybse-0.1b-src.zip">huskybse-0.1b-src.zip</a></td>
-            <td>older release huskybse-0.1b-stable, with example configuration files etc.</td>
+            <td><b>huskybase-1.0-src.tar.gz</b></td>
+            <td>huskybase v1.0</td>
           </tr>
+
           <tr valign="top">
-            <td><a href="http://cvs.physcip.uni-stuttgart.de/cvs2/current/huskybse-current-latest.tar.gz"><tt>huskybse-current-latest.tar.gz</tt></a></td>
-            <td>unstable development cvs snapshot</td>
+            <td><b>huskybase-1.0.0-1.noarch.rpm</b></td>
+            <td>RPM archive of huskybase v1.0</td>
           </tr>
+
+          <tr valign="top">
+            <td><b>huskybase-1.0.0-1.src.rpm</b></td>
+            <td>RPM Source archive of huskybase v1.0</td>
+          </tr>
+
         </table>
-      </blockquote>
 
-      <h3>Contacting the author</h3>
       <blockquote>
-        Maintained by Sascha M. Silbe  et. al.  See the <a href="team.html">team page</a>.
+      <p>The following releases are available in CVS.
+	<ul>
+            <li>huskybse-0.2-release</li>
+            <li>huskybse-0.1b-release</li>
+            <li>huskybse-0.1a-release</li>
+	</ul>
+      </blockquote>
+
+      <h3>Contacting the authors</h3>
+      <blockquote>
+        Maintained by Sascha M. Silbe  et. al.  See the&nbsp;<a href="team.html">team page</a>.
       </blockquote>
 
 
-      <small>Last update: 27/11/2001</small>
+      <small>Last update: 13/12/2006</small>
 
 #include "bottom.h"
