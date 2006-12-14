d7d7fd842cae3dcf2c4dd53ff8e585087f8cbe1e
Robert James Clay <jame@users.sourceforge.net>
2006-12-14 00:28:47 +0000

    Spelling edits, etc.  Updated Current info. Updated Debian info.

--- src/dl2.c
+++ src/dl2.c
@@ -5,20 +5,18 @@
 
       <blockquote>
 
-      <p><a href="current.html">Current</a> Husky is available from <a
-      href="/cvs2">
-      http://husky.sourceforge.net/cvs2</a>. Do not use it without good
-      reason.</p>
-
       <p>To get <a href="stable.html">stable</a> Husky versions, you can browse
-      to the individual entires of each programs (see below in the navigation
-      bar), or you can directly access our download page at <a
-      href="http://sourceforge.net/projects/husky/">
-      http://sourceforge.net/projects/husky/</a>.</p>
+      to the individual entries of each program (see over in the navigation
+      bar), or you can directly access our
+      <a href="http://sourceforge.net/projects/husky/">downloads</a>&nbsp;page.</p>
 
       <p>If you are using Debian GNU/Linux take a look at&nbsp;<a href="debian.html">
       our Debian-Page.</a>. You could install the Husky-Programs using the normal 
       and easy Debian-way (aptitude, apt-get, dselect, ...).</p>
+
+      <p><a href="current.html">Current</a> Husky is available from the Husky Project&nbsp;<a
+      href="http://sourceforge.net/cvs/?group_id=1332">CVS</a>. Do not use it without good
+      reason.</p>
 
       <p>If you are using BeOS take a look at <a href="http://walther.sf.net">
       Walther: Fidonet for BeOS</a> page.</p>
