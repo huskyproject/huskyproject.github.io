578859bf3c9f123dd1b3314d6b6a45dffd650d3f
Robert James Clay <jame@users.sourceforge.net>
2006-12-15 01:31:42 +0000

    Corrected CVS & downloads URL, & added to write up. Added date.

--- src/dl2.c
+++ src/dl2.c
@@ -10,15 +10,19 @@
       bar), or you can directly access our
       <a href="http://sourceforge.net/projects/husky/">downloads</a>&nbsp;page.</p>
 
-      <p>If you are using Debian GNU/Linux take a look at&nbsp;<a href="debian.html">
-      our Debian-Page.</a>. You could install the Husky-Programs using the normal 
+      <p>If you are using Debian GNU/Linux take a look at our&nbsp;<a href="debian.html">
+      Debian Page</a>. You could install the Husky Programs using the normal 
       and easy Debian-way (aptitude, apt-get, dselect, ...).</p>
 
-      <p><a href="current.html">Current</a> Husky is available from the Husky Project&nbsp;<a
-      href="http://sourceforge.net/cvs/?group_id=1332">CVS</a>. Do not use it without good
-      reason.</p>
+      <p><a href="current.html">Current</a>&nbsp;Husky is available from the Husky Project&nbsp;<a
+      href="http://sourceforge.net/cvs/?group_id=1332">CVS</a>, which is also viewable online at 
+        <a href="http://husky.cvs.sourceforge.net/husky/">
+        http://husky.cvs.sourceforge.net/husky/</a>.
+	  Do not use it without good reason.</p>
 
       <p>If you are using BeOS take a look at <a href="http://walther.sf.net">
       Walther: Fidonet for BeOS</a> page.</p>
+
+      <small>Last update: 14 December 2006</small>
 
 #include "bottom.h"
