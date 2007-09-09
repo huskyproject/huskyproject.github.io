9ca5d7054aa59e71e31aec6976a0c6eb9451970c
Robert James Clay <jame@users.sourceforge.net>
2007-09-09 19:00:24 +0000

    Updated msged links

--- src/msged.c
+++ src/msged.c
@@ -26,25 +26,29 @@
       <blockquote>
         <table cellpadding="5" cellspacing="0" border="0">
           <tr valign="top">
-            <td><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/msged-6.0.1-src.tar.gz"><tt>msged-6.0.1-src.tar.gz</tt></a></td>
+            <td><a href="http://www.physcip.uni-stuttgart.de/tobi/msged/archive/msge601s.zip"><tt>msge601s.zip</tt></a></td>
             <td>newest release, source code</td>
           </tr>
           <tr valign="top">
-            <td><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/msged-6.0.1-os2.zip"><tt>msged-6.0.1-os2.zip</tt></a></td>
+            <td><a href="http://www.physcip.uni-stuttgart.de/tobi/msged/archive/msge601o.zip"><tt>msge601o.zip</tt></a></td>
             <td>newest release, OS/2 version</td>
           </tr>
           <tr valign="top">
-            <td><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/msged-6.0.1-win.zip"><tt>msged-6.0.1-win.zip</tt></a></td>
+            <td><a href="http://www.physcip.uni-stuttgart.de/tobi/msged/archive/msge601w.zip"><tt>msge601w.zip</tt></a></td>
             <td>newest release, Windows version</td>
           </tr>
           <tr valign="top">
-            <td><a href="http://www.physcip.uni-stuttgart.de:8080/fidosoft.husky/msged-6.0.1-dos.zip"><tt>msged-6.0.1-dos.zip</tt></a></td>
+            <td><a href="http://www.physcip.uni-stuttgart.de/tobi/msged/archive/msge601d.zip"><tt>msge601d.zip</tt></a></td>
             <td>newest release, DOS version</td>
           </tr>
+
+<!--				9 Sep 2007 - not certain where this is still avaiable  
           <tr valign="top">
             <td><a href="http://www.physcip.uni-stuttgart.de/tobi/bin/MSGTE5_S.ZIP"><tt>MSGTE5_S.ZIP</tt></a></td>
             <td>very old release: msged te 05 source code</td>
           </tr>
+-->
+	  
           <tr valign="top">
             <td><a href="http://cvs.physcip.uni-stuttgart.de/cvs2/current/msged-current-latest.tar.gz"><tt>msged-current-latest.tar.gz</tt></a></td>
             <td>unstable development cvs snapshot</td>
@@ -65,6 +69,6 @@
       </blockquote>
 
 
-      <small>Last update: 10/02/2002</small>
+      <small>Last update: 9 September 2007</small>
 
 #include "bottom.h"
